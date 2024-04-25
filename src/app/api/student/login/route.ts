738e5c7b6164958f35531e9e7160b6b7d669a161
syket-das <saiketsd23@gmail.com>
2024-04-25 20:36:13 +0530

    sd

---
+++ src/app/api/student/login/route.ts
@@ -0,0 +1,52 @@
+import { prisma } from '@/lib/prisma'
+
+export async function POST(request: Request) {
+  try {
+    const { phone, password } = await request.json()
+
+    console.log(phone, password)
+
+    const student = await prisma.student.findUnique({
+      where: {
+        phone
+      }
+    })
+
+    if (!student) {
+      return Response.json({
+        success: false,
+        message: 'Student not found'
+      })
+    }
+
+    if (student.password !== password) {
+      return Response.json({
+        success: false,
+        message: 'Invalid password'
+      })
+    }
+
+    return Response.json({
+      success: true,
+      data: student
+    })
+  } catch (error) {
+    return Response.json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    return Response.json({
+      success: true
+    })
+  } catch (error) {
+    return Response.json({
+      success: false,
+      message: error.message
+    })
+  }
+}
